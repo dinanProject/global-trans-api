35948416d2d66aafbf504ba96a3dcd9b0f46b6e4
Dinan <96161679+PEI-Dinan-Adi@users.noreply.github.com>
2026-08-07 22:28:10 +0700

    feat: add email outbox monitoring and retry endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -24,6 +24,7 @@ route.use("/equipment-request", require("./equipment-request"));
 
 route.use("/file-attachment", require("./file-attachment/file-attachment"));
 route.use("/home", require("./home/dashboard"));
+route.use("/email", require("./administration/email"));
 route.use("/login-log", require("./log/login-log"));
 
 module.exports = route;
